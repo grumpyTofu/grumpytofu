8c3af0466cf6a007b4252e56c8626b4a52cb6715
grumpytofu <>
2023-01-24 22:24:27 -0500

    responsive design fixes

--- src/app/contact.tsx
+++ src/app/contact.tsx
@@ -7,12 +7,12 @@ const Contact = () => {
     <Section id="contact" variant="frosted" className="flex justify-center !min-h-0">
       <Card className="!-mt-20 z-10 !w-full !bg-slate-800">
         <div className="flex flex-col text-left w-10/12 sm:w-7/12 lg:w-5/12 mb-6 sm:mb-0">
-          <Typography variant="h3" className="!font-medium">
-            <span className="block">Ready to dive in?</span>
+          <Typography variant="h3" className="!font-medium text-center sm:text-start">
+            Ready to dive in?
           </Typography>
-          <Typography>Contact us to schedule a consultation.</Typography>
+          <Typography className="hidden md:block md:mt-3">Contact me to schedule a consultation.</Typography>
         </div>
-        <Link href="/contact">Contact</Link>
+        <Link href="/contact" className="text-2xl sm:text-xl lg:text-lg mt-6 sm:mt-3 md:mt-0">Contact</Link>
       </Card>
     </Section>
   );
