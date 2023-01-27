f1c498c2de25ea89175fddf20c567b88cc26156c
grumpytofu <>
2023-01-26 20:04:45 -0500

    undo all blogs - switching to medium

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import "./globals.css";
 import Footer from "@portfolio/components/footer";
-import { DataService } from "@portfolio/services/data.service";
-import { useQuery } from "react-query";
-import { use } from "react";
-
-const initDb = async () => {
-  const dataService = new DataService();
-  await dataService.init();
-  return dataService;
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  use(initDb());
-
   return (
     <html lang="en">
       <head />
