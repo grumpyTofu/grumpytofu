e1a4f7005baa4e03e9cfcbf310d1b3791cd0ffa2
grumpytofu <>
2023-02-05 17:42:31 -0500

    udpate analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Footer from "@portfolio/components/footer";
 import ForceScroll from "@portfolio/components/forceScroll";
 import Background from "@portfolio/components/background";
 import GoogleAnalytics from "@portfolio/components/google-analytics";
+import { AnalyticsWrapper } from "@portfolio/components/Analytics";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -11,6 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <title>GrumpyTofu</title>
       </head>
       <body>
+        <AnalyticsWrapper />
         <GoogleAnalytics />
         <Background />
         <ForceScroll />
