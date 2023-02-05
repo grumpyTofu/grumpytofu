73396a390a3fb85a580b6083bcb5ca5eca6a2f40
grumpytofu <>
2023-02-04 22:39:02 -0500

    update gtag based on nextjs example

--- additional.d.ts
+++ additional.d.ts
@@ -2,5 +2,6 @@ export declare global {
   interface Window {
     grecaptcha: any;
     dataLayer: any[] | undefined;
+    gtag: any;
   }
 }
--- src/components/google-analytics.tsx
+++ src/components/google-analytics.tsx
@@ -1,26 +1,56 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import Script from "next/script";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
+
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+class Gtag {
+  // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+  static pageview(url: string) {
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
+
+  // https://developers.google.com/analytics/devguides/collection/gtagjs/events
+  static event({ action, category, label, value }: any) {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
+}
 
 export const GoogleAnalytics = () => {
-  const gtag = (...args: any) => {
-    if (window.dataLayer) window.dataLayer.push(args);
+  const pathname = usePathname();
+
+  const handleRouteChange = (pathname: string) => {
+    Gtag.pageview(pathname);
   };
+
+  useEffect(() => handleRouteChange(pathname || ""), [pathname]);
 
   return (
     <Suspense fallback={<></>}>
-      <Script
-        async
-        strategy="lazyOnload"
-        src="https://www.googletagmanager.com/gtag/js?id=G-428LF2BSVJ"
-        onLoad={() => {
-          window.dataLayer = window.dataLayer || [];
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
 
-          gtag("js", new Date());
-          gtag("config", "G-428LF2BSVJ");
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
         }}
-      ></Script>
+      />
     </Suspense>
   );
 };
