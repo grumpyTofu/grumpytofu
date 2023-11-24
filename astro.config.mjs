a04e303d92c1b8d2335175ae0c2db016fb25b15b
grumpytofu <>
2023-11-24 00:57:07 -0500

    add qwik

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
-
 import cloudflare from '@astrojs/cloudflare';
+
+import qwikdev from "@qwikdev/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()],
+  integrations: [tailwind(), solidJs(), qwikdev()],
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare()
 });
